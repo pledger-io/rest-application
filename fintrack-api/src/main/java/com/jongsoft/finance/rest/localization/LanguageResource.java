6fc64ff3032796a24eacfd1e2e2a264dad067f1b
Gerben Jongerius <g.jongerius@jong-soft.com>
2021-12-27 17:03:59 +0100

    Add operation to fetch an entire language file at once.

--- fintrack-api/src/main/java/com/jongsoft/finance/rest/localization/LanguageResource.java
+++ fintrack-api/src/main/java/com/jongsoft/finance/rest/localization/LanguageResource.java
@@ -7,10 +7,15 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.inject.Inject;
 import lombok.RequiredArgsConstructor;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
 
 @Secured(SecurityRule.IS_ANONYMOUS)
 @Controller("/api/localization/lang")
@@ -18,6 +23,25 @@ import java.util.Locale;
 public class LanguageResource {
 
     private final MessageSource messageSource;
+
+    @Get("/{language}")
+    @Operation(
+            summary = "Get a localization file",
+            operationId = "getTranslations"
+    )
+    public Map<String, String> get(@PathVariable String language) throws IOException {
+        var pathPart = "en".equals(language) ? "" : "_" + language;
+
+        var messages = getClass().getResourceAsStream("/i18n/messages" + pathPart + ".properties");
+        var validation = getClass().getResourceAsStream("/i18n/ValidationMessages" + pathPart + ".properties");
+
+        var response = new HashMap<String, String>();
+        var textKeys = new Properties();
+        textKeys.load(messages);
+        textKeys.load(validation);
+        textKeys.forEach((key, value) -> response.put(key.toString(), value.toString()));
+        return response;
+    }
 
     @Get("/{language}/{textKey}")
     public LanguageResponse getText(@PathVariable String language, @PathVariable String textKey) {
