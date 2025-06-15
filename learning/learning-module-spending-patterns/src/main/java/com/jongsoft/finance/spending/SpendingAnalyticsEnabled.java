9e8969a7b66334e9cde110dbd7f002194dda1dea
Gerben Jongerius <g.jongerius@jong-soft.com>
2025-06-15 10:43:46 +0200

    Resolve conflict between test config and production h2 config.

--- learning/learning-module-spending-patterns/src/main/java/com/jongsoft/finance/spending/SpendingAnalyticsEnabled.java
+++ learning/learning-module-spending-patterns/src/main/java/com/jongsoft/finance/spending/SpendingAnalyticsEnabled.java
@@ -3,5 +3,4 @@ package com.jongsoft.finance.spending;
 import io.micronaut.context.annotation.Requires;
 
 @Requires(notEnv = "no-analytics")
-public @interface SpendingAnalyticsEnabled {
-}
+public @interface SpendingAnalyticsEnabled {}
