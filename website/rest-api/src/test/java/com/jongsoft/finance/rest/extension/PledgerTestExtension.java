5be2cff86bcb0e27a1ad4a6b3458c0bb13e04529
Gerben Jongerius <g.jongerius@jong-soft.com>
2025-10-03 17:07:45 +0200

    Update bank account tests.

--- website/rest-api/src/test/java/com/jongsoft/finance/rest/extension/PledgerTestExtension.java
+++ website/rest-api/src/test/java/com/jongsoft/finance/rest/extension/PledgerTestExtension.java
@@ -8,63 +8,74 @@ import com.jongsoft.finance.security.CurrentUserProvider;
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.context.event.ApplicationEventPublisher;
 import io.micronaut.test.extensions.junit5.MicronautJunit5Extension;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.extension.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.mockito.Mockito.mock;
 
 public class PledgerTestExtension implements ParameterResolver, BeforeAllCallback, AfterAllCallback {
 
-  private final static Logger log = LoggerFactory.getLogger(PledgerTestExtension.class);
-  private static final ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(PledgerTestExtension.class);
+    private static final ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(PledgerTestExtension.class);
 
-  @Override
-  public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-    return parameterContext.getParameter()
-        .getType()
-        .isAssignableFrom(PledgerContext.class);
-  }
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return parameterContext.getParameter()
+              .getType()
+              .isAssignableFrom(PledgerContext.class)
+              || parameterContext.getParameter()
+              .getType()
+              .isAssignableFrom(PledgerRequests.class);
+    }
 
-  @Override
-  public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-    return getStore(extensionContext)
-        .get(PledgerContext.class, PledgerContext.class);
-  }
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        if (parameterContext.getParameter().getType().isAssignableFrom(PledgerContext.class)) {
+            return getStore(extensionContext)
+                  .get(PledgerContext.class, PledgerContext.class);
+        }
+        if (parameterContext.getParameter().getType().isAssignableFrom(PledgerRequests.class)) {
+            return getStore(extensionContext)
+                  .get(PledgerRequests.class, PledgerRequests.class);
+        }
 
-  @Override
-  public void beforeAll(ExtensionContext context) {
-    var store = context.getRoot()
-        .getStore(ExtensionContext.Namespace.create(MicronautJunit5Extension.class));
-    var applicationContext = store.get(ApplicationContext.class, ApplicationContext.class);
+        throw new ParameterResolutionException("Unsupported parameter type: " + parameterContext.getParameter().getType());
+    }
 
-    applicationContext.registerSingleton(AuthenticationFacade.class, mock(AuthenticationFacade.class));
-    applicationContext.registerSingleton(CurrentUserProvider.class, mock(CurrentUserProvider.class));
-    applicationContext.registerSingleton(FinTrack.class, new FinTrack(new Encoder() {
-      @Override
-      public String encrypt(String value) {
-        return value;
-      }
+    @Override
+    public void beforeAll(ExtensionContext context) {
+        var store = context.getRoot()
+              .getStore(ExtensionContext.Namespace.create(MicronautJunit5Extension.class));
+        var applicationContext = store.get(ApplicationContext.class, ApplicationContext.class);
 
-      @Override
-      public boolean matches(String encoded, String value) {
-        return encoded.equals(value);
-      }
-    }));
+        applicationContext.registerSingleton(AuthenticationFacade.class, mock(AuthenticationFacade.class));
+        applicationContext.registerSingleton(CurrentUserProvider.class, mock(CurrentUserProvider.class));
+        applicationContext.registerSingleton(FinTrack.class, new FinTrack(new Encoder() {
+            @Override
+            public String encrypt(String value) {
+                return value;
+            }
 
-    new EventBus(applicationContext.getBean(ApplicationEventPublisher.class));
+            @Override
+            public boolean matches(String encoded, String value) {
+                return encoded.equals(value);
+            }
+        }));
 
-    getStore(context)
-        .put(PledgerContext.class, new PledgerContext(applicationContext));
-  }
+        new EventBus(applicationContext.getBean(ApplicationEventPublisher.class));
 
-  @Override
-  public void afterAll(ExtensionContext extensionContext) {
-    getStore(extensionContext)
-        .get(PledgerContext.class, PledgerContext.class).reset();
-  }
+        getStore(context)
+              .put(PledgerContext.class, new PledgerContext(applicationContext));
+        getStore(context)
+              .put(PledgerRequests.class, new PledgerRequests(applicationContext.getBean(RequestSpecification.class)));
+    }
 
-  private static ExtensionContext.Store getStore(ExtensionContext extensionContext) {
-    return extensionContext.getRoot().getStore(NAMESPACE);
-  }
+    @Override
+    public void afterAll(ExtensionContext extensionContext) {
+        getStore(extensionContext)
+              .get(PledgerContext.class, PledgerContext.class).reset();
+    }
+
+    private static ExtensionContext.Store getStore(ExtensionContext extensionContext) {
+        return extensionContext.getRoot().getStore(NAMESPACE);
+    }
 }
