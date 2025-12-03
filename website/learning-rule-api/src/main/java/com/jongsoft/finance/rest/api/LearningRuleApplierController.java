3fe83af82156102fab4c2b51f1375084707c46c3
Gerben Jongerius <g.jongerius@jong-soft.com>
2025-12-03 14:22:44 +0100

    Add transaction extract logic back in.

--- website/learning-rule-api/src/main/java/com/jongsoft/finance/rest/api/LearningRuleApplierController.java
+++ website/learning-rule-api/src/main/java/com/jongsoft/finance/rest/api/LearningRuleApplierController.java
@@ -1,10 +1,12 @@
 package com.jongsoft.finance.rest.api;
 
+import com.jongsoft.finance.core.exception.StatusException;
 import com.jongsoft.finance.learning.SuggestionEngine;
 import com.jongsoft.finance.learning.SuggestionInput;
 import com.jongsoft.finance.learning.SuggestionResult;
+import com.jongsoft.finance.learning.TransactionResult;
 import com.jongsoft.finance.rest.LearningRuleApplierApi;
-import com.jongsoft.finance.rest.model.rule.SuggestClassifications200Response;
+import com.jongsoft.finance.rest.model.rule.*;
 
 import io.micronaut.http.annotation.Controller;
 
@@ -36,6 +38,27 @@ class LearningRuleApplierController implements LearningRuleApplierApi {
         response.setBudget(suggested.budget());
         response.setCategory(suggested.category());
         response.setTags(suggested.tags());
+        return response;
+    }
+
+    @Override
+    public ExtractedResponse extractTransaction(
+            ExtractTransactionRequest extractTransactionRequest) {
+
+        TransactionResult extracted = suggestionEngine
+                .extractTransaction(extractTransactionRequest.getText())
+                .orElseThrow(() ->
+                        StatusException.badRequest("Unable to extract transaction from text."));
+
+        ExtractedResponse response = new ExtractedResponse();
+        response.setDate(extracted.date());
+        response.setAmount(extracted.amount());
+        response.setDescription(extracted.description());
+        response.setFrom(new ExtractedResponseFrom(
+                extracted.from().id(), extracted.from().name()));
+        response.setTo(
+                new ExtractedResponseFrom(extracted.to().id(), extracted.to().name()));
+        response.setType(ExtractedResponseType.valueOf(extracted.type().name()));
         return response;
     }
 }
