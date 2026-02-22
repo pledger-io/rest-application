aaf0dccf359f295bb776f6d18cd400121014bfa1
Gerben Jongerius <g.jongerius@jong-soft.com>
2026-02-22 14:43:24 +0100

    Update langchain4j to 1.11.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -38,8 +38,8 @@ dependencyResolutionManagement {
         }
 
         create("llm") {
-            val langchain4jVersion: String = "1.10.0"
-            val betaVersion: String = "$langchain4jVersion-beta18"
+            val langchain4jVersion: String = "1.11.0"
+            val betaVersion: String = "$langchain4jVersion-beta19"
             library("core", "dev.langchain4j", "langchain4j").version(langchain4jVersion)
             library("retriever-sql", "dev.langchain4j", "langchain4j-pgvector").version(betaVersion)
             library("store", "dev.langchain4j", "langchain4j-embeddings-all-minilm-l6-v2").version(betaVersion)
