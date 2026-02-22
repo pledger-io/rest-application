8c2b27c9f35af14d04efd52cf4beffbad9e86e54
Gerben Jongerius <g.jongerius@jong-soft.com>
2026-02-22 13:17:35 +0100

    Rmove no longer needed files

--- bpmn-process/src/main/java/com/jongsoft/finance/ProcessVariable.java
+++
@@ -1,6 +0,0 @@
-package com.jongsoft.finance;
-
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "_type")
-public interface ProcessVariable {}
