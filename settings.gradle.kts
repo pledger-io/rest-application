3033348ca2343305804ec0b817ab574587f732a0
Gerben Jongerius <g.jongerius@jong-soft.com>
2026-02-22 14:24:53 +0100

    Remove openapi generator

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,6 @@ pluginManagement {
         id("io.micronaut.library").version("4.6.1")
         id("io.micronaut.application").version("4.6.1")
         id("org.sonarqube").version("7.2.2.6593")
-        id("org.openapi.generator").version("7.18.0")
         id("com.diffplug.spotless").version("8.2.1")
         id("io.micronaut.openapi").version("4.5.4")
 
