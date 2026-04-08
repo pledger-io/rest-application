bcea35847c368aa2251e78d9999c82bb28f85f03
Gerben Jongerius <g.jongerius@jong-soft.com>
2026-04-08 17:22:22 +0200

    Update langchain4j dependency to beta22 in build script.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -39,7 +39,7 @@ dependencyResolutionManagement {
 
         create("llm") {
             val langchain4jVersion: String = "1.12.2"
-            val betaVersion: String = "$langchain4jVersion-beta19"
+            val betaVersion: String = "$langchain4jVersion-beta22"
             library("core", "dev.langchain4j", "langchain4j").version(langchain4jVersion)
             library("retriever-sql", "dev.langchain4j", "langchain4j-pgvector").version(betaVersion)
             library("store", "dev.langchain4j", "langchain4j-embeddings-all-minilm-l6-v2").version(betaVersion)
