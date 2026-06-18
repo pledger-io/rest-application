fa1b56587b28d758a58dd30ab50cb124d91d6fba
Gerben Jongerius <g.jongerius@jong-soft.com>
2026-06-18 15:31:32 +0200

    Add missing module in checks

--- src/main/java/com/jongsoft/finance/core/adapter/rest/SecurityController.java
+++ src/main/java/com/jongsoft/finance/core/adapter/rest/SecurityController.java
@@ -67,7 +67,8 @@ class SecurityController implements SecurityApi {
                 .get();
         Properties registrationProperties = new Properties();
         registrationProperties.put("user", userAccount);
-        mailDaemon.send(userAccount.getUsername().email(), "user-registered", registrationProperties);
+        mailDaemon.send(
+                userAccount.getUsername().email(), "user-registered", registrationProperties);
 
         return HttpResponse.noContent();
     }
